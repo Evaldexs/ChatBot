8d7154f3e161dc2a2e772e7ba33c569f099f53fe
Mircea Finaru (finarum) <finarum@coventry.ac.uk>
2018-11-26 10:42:27 +0000

    Add files via upload

--- finaru.py
+++ finaru.py
@@ -2,6 +2,12 @@ import discord
 from discord.ext import commands
 import requests
 
+client = commands.Bot(command_prefix='/')
+
+#this part of the code is from the twitter api docs
+import twitter
+api = twitter.Api('tncvlizYNuoPCFrc2BbKNn3bS','TP3ZAqe2NRigShnHeH2vKctgwlay01D27DOqcB4ugeyTDtsSoi','1061914001523818496-EgqAXwVpo8gJMkLoWVYz3alSRkYnNS','W7D0UbAj4OlPdsAgjHnkDVTDnpNsuS2hVDsKewF6rPbDz')
+
 #this part of the code is from the TMDb docs
 from tmdbv3api import TMDb
 tmdb = TMDb()
@@ -11,6 +17,22 @@ from tmdbv3api import Movie
 class FinaruCog:
     def __init__(self, client):
         self.client = client
+
+    @commands.command()
+    async def toptweets(self, ctx, name: str):
+        #using Twitter API
+        await ctx.send("Giving you the latest 5 tweets on the following : #" + name)
+        results = api.GetSearch(raw_query="l=en&q=%23" + name + "%20&result_type=recent&since=2014-07-19&count=6")
+        for result in results:
+            await ctx.send("The tweet was created at " + result.created_at)
+            await ctx.send("https://twitter.com/" + str(result.user.id) + "/status/" + str(result.id_str))
+            #await ctx.send(result.id)
+            #await ctx.send(result.id_str)
+            #await ctx.send(result.text)
+            #await ctx.send(result.user)
+            #await ctx.send(result.entities)
+        
+    
     @commands.command()
     async def movie(self, ctx, name: str):
         #using TMDb API
@@ -29,9 +51,9 @@ class FinaruCog:
                         await ctx.send("https://image.tmdb.org/t/p/w500" + results.poster_path + "\n")
                         i= i+1
 
-    @commands.command()
+    
     async def movies(self, ctx):
-        await ctx.send("Giving you the most popular films")
+        await ctx.send("Giving you the most popular movies")
         movie = Movie()
         popular = movie.popular()
         i=0
@@ -47,50 +69,45 @@ class FinaruCog:
                 break
     
     async def on_message(ctx, message):
-        if message.content.startswith("/"):
-            ok=1
-        else:
-            mylist=message.content.split()
-            case=0
-            no=0
-            for words in mylist:
-                def valid(words):
-                    word = ""
-                    for character in words:
-                        if character.isalpha():
-                            word+=character
-                    return word
-                word=valid(words)
-                m = ["movie","movies"]
-                ms = ["music", "song"]
-                d = ["don't","dont","no"]
-                if word.lower() in m:
-                    case=1
-                elif word.lower() in ms:
-                    case=2
-                elif word.lower() in d:
-                    no=1
+        if (message.author.bot == False):
+            if message.content.startswith("/") == False:
+                if message.content.startswith("go"):
+                    await ctx.movies(message.channel)
+                mylist=message.content.split()
+                case=0
+                no=0
+                sim=0
+                for words in mylist:
+                    def valid(words):
+                        word = ""
+                        for character in words:
+                            if character.isalpha():
+                                word+=character
+                        return word
+                    word=valid(words)
+                    m = ["movie","movies"]
+                    ms = ["music", "song"]
+                    d = ["don't","dont","no"]
+                    if word.lower() in m:
+                        case=1
+                    elif word.lower() in ms:
+                        case=2
+                    elif word.lower() in d:
+                        no=1
+                    elif word.lower() == "similar":
+                        sim=1
+                    else:
+                        continue
+                if no == 1 and case != 0:
+                    await message.channel.send("What else can I do for you?")
                 else:
-                    continue
-            if no == 1 and case != 0:
-                await message.channel.send("What else can I do for you?")
-            else:
-                if case == 1:
-                    movie = Movie()
-                    popular = movie.popular()
-                    i=0
-                    for p in popular:
-                        if i<10:
-                            await message.channel.send(str(i+1) + " : ")
-                            await message.channel.send("ID: " + str(p.id) + "\n")
-                            await message.channel.send("Title: " + str(p.title) + "\n")
-                            await message.channel.send("Overview: " + str(p.overview) + "\n")
-                            await message.channel.send("https://image.tmdb.org/t/p/w500" + p.poster_path + "\n")
-                            i=i+1
+                    if case == 1:
+                        if sim == 0:
+                            await ctx.movies(message.channel)
                         else:
-                            break
-                elif case == 2:
-                    await message.channel.send("Searching for songs \n")
+                            await message.channel.send("If you want a similar match for a certain movie, use the '/movie name' command and I'll give you 5 similar matches for your movie")
+                    elif case == 2:
+                        await message.channel.send("Searching for songs \n")
 
 def setup(client):
     client.add_cog(FinaruCog(client))

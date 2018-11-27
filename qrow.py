8c82e6439d27ace764b10b1a0004b7d64b365a54
Hanyi Feng (fengh9) <fengh9@coventry.ac.uk>
2018-11-27 14:32:53 +0000

    Add files via upload

--- qrow.py
+++ qrow.py
@@ -14,16 +14,13 @@ class QrowCog:
 
 		a = ["How are you", "how are you", "How are you?","how are you?"]
 		b = ["How do you work", "how do you work", "How do you work?", "how do you work?"]
-		c = ["How to finish my homework", "how to finish my homework", "How to finish my homework?", "how to finish my homework?"]
+		c = ["How to finish my homework", "how to finish my homework", "How to finish my homework?", "how to finish my homework?"] 
 		d = ["How to pass the exam", "how to pass the exam", "How to pass the exam?","how to pass the exam?"]
 		e = ["How to find a free moive", "how to find a free moive", "How to find a free moive?", "how to find a free moive?"]
-                f = ["How do you think about me", "how do you think about me", "How do you think about me?", "how do you think about me?"]
-                g = ["How do you think about youself","how do you think about youself","How do you think about youself?","how do you think about youself?"]
+		f = ["How do you think about me", "how do you think about me", "How do you think about me?", "how do you think about me?"]
+		g = ["How do you think about youself","how do you think about youself","How do you think about youself?","how do you think about youself?"]
 
-                ff = ["Not bad.","Pretty good.","Sorry, I think we are not familiar yet."]
-                gg = ["I am just a virtual chatbot.", "I don't like to talk about myself.","I'm pretty cool."]
-                jj = ["It's your opinion that counts.", "I'm sorry I couldn't answer it.", "I really couldn't say."]
-                if message.content in a:
+		if message.content in a:
 			await message.channel.send ("Great,What about you?")
 		elif message.content in x:
 			random_xx = random.choice(xx)
@@ -31,21 +28,17 @@ class QrowCog:
 		elif message.content in b:
 			await message.channel.send ("That's a secret.") 
 		elif message.content in c:
-			await message.channel.send ("Hardworking, please!")
+			await message.channel.send ("Hardworking,please!")
 		elif message.content in d: 
 			await message.channel.send ("Only you can help yourself.")
 		elif message.content in e:
 			await message.channel.send ("Please support genuine!") 
 		elif message.content in f:
-                        random_ff = random.choice(ff)
-			await message.channel.send (str(random_ff)) 
-		elif message.content in g:
-                        random_gg = random.choice(gg)
-			await message.channel.send (str(random_gg))
-                else:
-                        random_jj = random.choice(jj)
-			await message.channel.send (str(random_jj))
+			await message.channel.send ("Not bad.") 
+		elif message.content in g: 
+			await message.channel.send ("I am just a virtual chatbot.")
+		else:
+			pass
 
-def tup(client):
-        client.add_cog(QrowCog(client)
-		
+def setup(client):
+	client.add_cog(QrowCog(client))
